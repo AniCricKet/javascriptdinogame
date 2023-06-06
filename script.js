c206b2684e1931285ff6f3ddeb93dfe465378c42
Aniket Chakradeo <aniket@chakradeo.net>
2023-06-06 10:56:32 -0700

    updates to comments

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-// Get the dinosaur, cactus, and jump button elements from the html file
+// Get the dinosaur, cactus, jump button, and score elements from the webpage
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 const jumpButton = document.getElementById("jumpButton");
+const scoreElement = document.getElementById("score");
+
+let score = 0; // Initialize the score to 0
 
 // Function that makes the dinosaur jump
 function jump() {
   // Check if the dinosaur is already jumping
   if (!dino.classList.contains("jump")) {
-    // If it is, make the dinosaur jump by adding the "jump" class
+    // Make the dinosaur jump by adding the "jump" class
     dino.classList.add("jump");
-    // After 300 milliseconds, remove the "jump" class to make the dinosaur come back down
+    // After 500 milliseconds, remove the "jump" class to make the dinosaur come back down
     setTimeout(function () {
       dino.classList.remove("jump");
     }, 500);
@@ -31,20 +34,34 @@ jumpButton.addEventListener("click", jump);
 // Add a keydown event listener to the document, so when any key is pressed, the handleKeyDown function is called
 document.addEventListener("keydown", handleKeyDown);
 
-// Set up a continuous check to detect if the dinosaur collides with the cactus
+// Set up a continuous check to detect if the dinosaur collides with the cactus or if the cactus hits the left side of the screen
 let isAlive = setInterval(function () {
   // Get the vertical position of the dinosaur
   let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
   // Get the horizontal position of the cactus
   let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+  // Check if the cactus hits the left side of the screen
+  if (cactusLeft < 0) {
+    // If the dino jumps over the cactus it will increment the score
+    score++;
+    scoreElement.textContent = score; // Update the score on the webpage
+  }
+
   // Check if the cactus is within a certain range of the dinosaur and if the dinosaur is at the appropriate height to collide
   if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
     // If there is a collision, stop the continuous check
     clearInterval(isAlive);
+
     // Stop the dinosaur's jumping animation
     dino.style.animation = "none";
+
     // Show an alert with the message "Game Over!"
     alert("Game Over!");
+
+    // Reset the score to 0
+    score = 0;
+    scoreElement.textContent = score; // Update the score on the webpage
+
     // Refresh the page to restart the game
     location.reload();
   }
